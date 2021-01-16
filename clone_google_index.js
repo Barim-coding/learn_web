0c00e0fc9bd1493caca878d1a3e42751c09712fe
Gradate <jungjh9707@naver.com>
2021-01-16 20:59:12 +0900

    1. update index.js   (1) rename to clone_google_index

--- clone_google_index.js
+++ clone_google_index.js
@@ -2,18 +2,21 @@ const Searching = Search.prototype;
 
 function Search() {
     this.keyword = document.querySelector('input[name="q"]');
-    this.form = document.querySelector('.button');
+    this.button = document.querySelector('.google_search')
+    this.form = document.querySelector('.text');
+
 
     this.Engine();
 }
 
-Searching.Search = function() {
-    this.form.addEventListener('submit', a => {
+Searching.Engine = function() {
+    this.form.addEventListener('submit', e => {
         e.preventDefault();
 
         let keyword = this.keyword.value;
 
         location.href = 'https://www.google.co.kr/search?q=' + keyword;
     });
+
 }
 new Search();
