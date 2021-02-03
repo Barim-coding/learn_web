ef6d78ebe3a15ba6ea5836e4b9a9b3816546c997
Gradate <jungjh9707@naver.com>
2021-02-03 16:07:24 +0900

    update some tag

--- clone_google_index.js
+++ clone_google_index.js
@@ -1,22 +1,3 @@
-$(document).ready(function() {
-    var search = function() {
-        var url = 'https://www.google.com/search?q=' + document.getElementById('search_text').value;
-        window.open(url, 'google');
-    };
-
-    var doodle = function() {
-        url = 'https://www.google.com/doodles'
-        window.open(url, 'google')
-
-    };
-
-    $(".gsearch").click(search);
-    $(".doodle").click(doodle);
-
-    $("#text").keypress(function(e) {
-        if (e.which == 13) {
-            search();
-        }
-    });
-
+$(".Im_feeling_lucky").click(function() {
+    location.href = 'https://www.google.com/doodles';
 });
